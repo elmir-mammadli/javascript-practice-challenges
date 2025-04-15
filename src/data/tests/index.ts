d7ed40b4b843136e13a55ab858d2c42215be4dcd
Elmir Mammadli <elmirmammadli18@gmail.com>
2025-04-15 00:52:04 -0400

    Add smallestAndBiggest and countConsonants tests to the test suite

--- src/data/tests/index.ts
+++ src/data/tests/index.ts
@@ -2,13 +2,17 @@ import { bookSortingTest } from './bookSorting';
 import { spyCodeTest } from './spyCode';
 import { pineappleCountTest } from './pineappleCount';
 import { lessThan99Test } from './lessThan99';
+import { smallestAndBiggestTest } from './smallestAndBiggest';
+import { countConsonantsTest } from './countConsonants';
 import { TestData } from '@/types';
 
 export const tests: TestData[] = [
   bookSortingTest,
   pineappleCountTest,
   spyCodeTest,
-  lessThan99Test
+  lessThan99Test,
+  smallestAndBiggestTest,
+  countConsonantsTest
 ];
 
 export const getTestById = (id: string): TestData | undefined => {
